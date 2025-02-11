7e97f7d1251ff8c45ea2b9424f7c13d0b2320fee
Sanjana  Pansara <139979917+sanjanapansara@users.noreply.github.com>
2025-02-11 17:19:51 +0530

    payment modal error solve

--- dbm-reseller/src/component/Webinstances/WebInstances.jsx
+++ dbm-reseller/src/component/Webinstances/WebInstances.jsx
@@ -22,6 +22,9 @@ import {
   Badge,
   Select,
   DatePicker,
+  Image,
+  message,
+  Alert,
 } from "antd";
 import {
   SearchOutlined,
@@ -101,9 +104,9 @@ const WebInstance = ({ isLogin }) => {
   const currency = useSelector((state) => state?.setting?.currency);
   const panel = useSelector((state) => state?.setting?.panel);
   const navigate = useNavigate();
-  const [plan, setPlan] = useState('1 Month');
+  const [plan, setPlan] = useState("1 Month");
   const [quantity, setQuantity] = useState(1);
-  const [paymentMethod, setPaymentMethod] = useState('Razor Pay');
+  const [paymentMethod, setPaymentMethod] = useState("Razor Pay");
   const pricePerInstance = 1490;
   const handlingChargeRate = 0.02;
   const subtotal = pricePerInstance * quantity;
@@ -131,13 +134,20 @@ const WebInstance = ({ isLogin }) => {
   const [status, setStatus] = useState("all");
   const [renew, setRenew] = useState("all");
   const [isApplyFilter, setIsApplyFilter] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+  const [showProductModal, setShowProductModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [selectedKeys, setSelectedKeys] = useState(10);
+  const [selectedValidity, setSelectedValidity] = useState(365);
+  const [razorpayInstance, setRazorpayInstance] = useState(null);
+
   //pagination
   const [page, setPage] = useState(1);
   const [total1, setTotal1] = useState(0);
   const [pageSize, setPageSize] = useState(10);
 
   const handleFilterChange = (status) => {
-    setStatusFilter(status);
     if (status === "all") {
       setFilteredData(dataSource);
     } else {
@@ -192,14 +202,15 @@ const WebInstance = ({ isLogin }) => {
   };
 
   const paymentGateways = useMemo(() => {
-    const currencyData = panel?.currencies?.find((cur) => cur?.code === currency);
+    const currencyData = panel?.currencies?.find(
+      (cur) => cur?.code === currency
+    );
     if (currencyData) {
       return currencyData?.paymentGateways;
     } else {
       return [];
     }
   }, [panel, currency]);
-
 
   useEffect(() => {
     if (paymentGateways.length > 0) {
@@ -444,17 +455,132 @@ const WebInstance = ({ isLogin }) => {
       ]}
     />
   );
+  const createOrder = async () => {
+    try {
+      const payment = {
+        currency: currency,
+        product_id: selectedProduct.id,
+        gateway: selectedPaymentGateway,
+        variation_id: selectedVariation._id,
+      };
+      const { data } = await axiosInstance.post(
+        "order/reseller/buy-license-keys",
+        payment
+      );
+      return data;
+    } catch (err) {
+      return {
+        status: false,
+        message: err.message,
+      };
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  const menu = (
-    <Menu
-      items={[
-        {
-          key: "1",
-          label: <Text>Download CSV</Text>,
-        },
-      ]}
-    />
-  );
+  const razorpayVerification = async (
+    order_id,
+    payment_id,
+    signature,
+    orderId
+  ) => {
+    try {
+      setLoading(true);
+      const { data } = await axiosInstance.post("order/razorpay-verification", {
+        order_id,
+        payment_id,
+        signature,
+      });
+      if (data.status) {
+        message.success(data?.message || "Payment verified successfully");
+        navigate(`/payment-success?id=${orderId}`);
+        setShowProductModal(false);
+      } else {
+        message.error(data.message || "Verify payment failed");
+        navigate(`/payment-failed?id=${orderId}`);
+      }
+    } catch (e) {
+      message.error(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const displayRazorpay = (orderResponse) => {
+    if (razorpayInstance) {
+      const existingRazorpayForm = document.getElementById("razorpay-form");
+      if (existingRazorpayForm) {
+        existingRazorpayForm.remove();
+      }
+    }
+
+    const options = {
+      key: orderResponse?.result?.razorPay?.keyId,
+      amount: orderResponse?.result?.order?.total,
+      currency: orderResponse?.result?.currency,
+      name: panel?.reseller?.name,
+      description: "Order #" + orderResponse.result?.order?.id,
+      image: `${getMediaPath(panel?.reseller?.logo)}`,
+      order_id: orderResponse.result?.razorPay?.orderId,
+      handler: (response) => {
+        razorpayVerification(
+          response.razorpay_order_id,
+          response.razorpay_payment_id,
+          response.razorpay_signature,
+          orderResponse.result?.order?.id
+        );
+      },
+      prefill: {
+        name: profile ? profile.name : "",
+        email: profile ? profile.email : "",
+        contact: profile ? profile.phone : "",
+      },
+      notes: {
+        //address: "Buy License Keys",
+      },
+      theme: {
+        //color: "#1677FF",
+      },
+    };
+
+    const newRazorpayInstance = new Razorpay(options);
+    setRazorpayInstance(newRazorpayInstance);
+
+    newRazorpayInstance.on("payment.failed", function (response) {
+      message.error(response.error.code + ": " + response.error.description);
+    });
+
+    newRazorpayInstance.open();
+  };
+
+  const handlePaymentMethod = async () => {
+    try {
+      setLoading(true);
+      const orderResponse = await createOrder();
+      if (orderResponse?.status) {
+        if (selectedPaymentGateway === "razorPay") {
+          displayRazorpay(orderResponse);
+        } else if (selectedPaymentGateway === "payPal") {
+          if (orderResponse?.result?.payPal?.url) {
+            window.location.href = orderResponse?.result?.payPal?.url;
+          }
+        } else if (selectedPaymentGateway === "stripe") {
+          if (orderResponse?.result?.stripe?.url) {
+            window.location.href = orderResponse?.result?.stripe?.url;
+          }
+        }
+      } else {
+        message.error(orderResponse?.message);
+      }
+    } catch (error) {
+      console.error("Payment error:", error);
+      message.error(
+        "An error occurred while processing your payment. Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
       <ConfigProvider
@@ -522,8 +648,7 @@ const WebInstance = ({ isLogin }) => {
                 <Button
                   type={statusFilter == "all" ? "primary" : "default"}
                   onClick={() => {
-                    // setStatusFilter("all"),
-                    handleFilterChange("all");
+                    setStatusFilter("all"), handleFilterChange("all");
                   }}
                   block
                 >
@@ -531,7 +656,10 @@ const WebInstance = ({ isLogin }) => {
                 </Button>
                 <Button
                   type={statusFilter == "connected" ? "primary" : "default"}
-                  onClick={() => handleFilterChange("connected")}
+                  onClick={() => {
+                    setStatusFilter("connected"),
+                      handleFilterChange("connected");
+                  }}
                   block
                 >
                   {t("Connected")}
@@ -540,6 +668,7 @@ const WebInstance = ({ isLogin }) => {
                   type={statusFilter == "disconnected" ? "primary" : "default"}
                   onClick={() => {
                     handleFilterChange("disconnected");
+                    setStatusFilter("disconnected");
                   }}
                   block
                 >
@@ -561,20 +690,24 @@ const WebInstance = ({ isLogin }) => {
             >
               <Flex gap="small" justify="space-evenly">
                 <Button
-                  type={statusFilter == "claim trial" ? "primary" : "default"}
-                  onClick={() => setStatusFilter("available")}
+                  // type={statusFilter == "claim trial" ? "primary" : "default"}
+                  // onClick={() => setStatusFilter("available")}
                   icon={<CheckCircleOutlined />}
                   block
                 >
                   {t("Claim trial")}
                 </Button>
                 <Button
-                  type={statusFilter == "add" ? "primary" : "default"}
-                  onClick={() => setStatusFilter("active")}
+                  // type={statusFilter == "add" ? "primary" : "default"}
+                  // onClick={() => setStatusFilter("active")}
+                  onClick={() => {
+                    // resetFilterParameters();
+                    showModal4();
+                  }}
                   block
                   icon={<PlusOutlined />}
                 >
-                  <PlusOutlined /> {t("Add")}
+                  {t("Add")}
                 </Button>
                 {/* <Button
                   type={statusFilter == "expired" ? "primary" : "default"}
@@ -870,17 +1003,13 @@ const WebInstance = ({ isLogin }) => {
       <Modal
         title=""
         open={isModalOpen4}
-        onOk={handleOk4}
+        onOk={handlePaymentMethod}
         onCancel={handleCancel4}
-        okText="Confirm"
+        okText="Pay"
         cancelText="Cancel"
         centered
         width={500}
-        footer={[
-          <Button key="back" onClick={handleCancel4}>
-            Cancel
-          </Button>,
-        ]}
+       
       >
         <Title level={4}>Buy Web Instance</Title>
 
@@ -926,7 +1055,44 @@ const WebInstance = ({ isLogin }) => {
             <Text strong>Payment Method</Text>
           </Col>
           <Col span={24}>
-            <Radio.Group
+            {selectedPaymentGateway === null ? (
+              <Alert
+                type="warning"
+                message={`Payment method is not available for currency ${currency}`}
+              />
+            ) : (
+              <Radio.Group
+                optionType="button"
+                size="large"
+                block
+                onChange={(e) => setSelectedPaymentGateway(e.target.value)}
+                value={selectedPaymentGateway}
+              >
+                <Space>
+                  {paymentGateways?.map((gateway) => (
+                    <Radio.Button
+                      key={gateway}
+                      value={gateway}
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Image
+                        src={getMediaPath(
+                          `/media/payment-gateway/${gateway}.png`
+                        )}
+                        preview={false}
+                        alt={gateway}
+                        width={80}
+                      />
+                    </Radio.Button>
+                  ))}
+                </Space>
+              </Radio.Group>
+            )}
+            {/* <Radio.Group
               value={paymentMethod}
               onChange={(e) => setPaymentMethod(e.target.value)}
             >
@@ -949,7 +1115,7 @@ const WebInstance = ({ isLogin }) => {
                     </Space>
                 <Radio.Button value="Paytm">Paytm</Radio.Button>
               </Space>
-            </Radio.Group>
+            </Radio.Group> */}
           </Col>
         </Row>
 
@@ -977,12 +1143,14 @@ const WebInstance = ({ isLogin }) => {
           </Col>
         </Row>
 
-        <Row justify="end" style={{ marginTop: 16 }}>
+        {/* <Row justify="end" style={{ marginTop: 16 }}>
           <Space>
             <Button>Cancel</Button>
-            <Button type="primary">Pay</Button>
+            <Button type="primary" onClick={handlePaymentMethod}>
+              Pay
+            </Button>
           </Space>
-        </Row>
+        </Row> */}
       </Modal>
     </>
   );
